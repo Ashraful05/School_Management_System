f77a30165bae545d0137a54a9f40e88afab267e8
Ashraful05 <jamalrasu420@gmail.com>
2024-01-28 19:47:17 +0600

    login page customized

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -7,16 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="{{ asset('/') }}admin/images/favicon.ico">
+    <link rel="icon" href="../images/favicon.ico">
 
-    <title>School Admin - Log in </title>
+    <title>Sunny Admin - Log in </title>
 
     <!-- Vendors Style-->
-    <link rel="stylesheet" href="{{ asset('/') }}admin/css/vendors_css.css">
+    <link rel="stylesheet" href="css/vendors_css.css">
 
     <!-- Style-->
-    <link rel="stylesheet" href="{{ asset('/') }}admin/css/style.css">
-    <link rel="stylesheet" href="{{ asset('/') }}admin/css/skin_color.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/skin_color.css">
 
 </head>
 <body class="hold-transition theme-primary bg-gradient-primary">
@@ -32,14 +32,13 @@
                         <p class="text-white-50">Sign in to start your session</p>
                     </div>
                     <div class="p-30 rounded30 box-shadowed b-2 b-dashed">
-                        <form action="{{ route('login') }}" method="post">
-                            @csrf
+                        <form action="index.html" method="post">
                             <div class="form-group">
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text bg-transparent text-white"><i class="ti-user"></i></span>
                                     </div>
-                                    <input type="email" id="email" name="email" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="email">
+                                    <input type="text" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Username">
                                 </div>
                             </div>
                             <div class="form-group">
@@ -47,7 +46,7 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text  bg-transparent text-white"><i class="ti-lock"></i></span>
                                     </div>
-                                    <input type="password" id="password" name="password" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Password">
+                                    <input type="password" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Password">
                                 </div>
                             </div>
                             <div class="row">
@@ -60,7 +59,7 @@
                                 <!-- /.col -->
                                 <div class="col-6">
                                     <div class="fog-pwd text-right">
-                                        <a href="{{ route('password.request') }}" class="text-white hover-info"><i class="ion ion-locked"></i> Forgot pwd?</a><br>
+                                        <a href="javascript:void(0)" class="text-white hover-info"><i class="ion ion-locked"></i> Forgot pwd?</a><br>
                                     </div>
                                 </div>
                                 <!-- /.col -->
@@ -82,7 +81,7 @@
                         </div>
 
                         <div class="text-center">
-                            <p class="mt-15 mb-0 text-white">Don't have an account? <a href="{{ route('register') }}" class="text-info ml-5">Sign Up</a></p>
+                            <p class="mt-15 mb-0 text-white">Don't have an account? <a href="auth_register.html" class="text-info ml-5">Sign Up</a></p>
                         </div>
                     </div>
                 </div>
@@ -93,8 +92,8 @@
 
 
 <!-- Vendor JS -->
-<script src="{{ asset('/') }}admin/js/vendors.min.js"></script>
-<script src="{{ asset('/') }}assets/icons/feather-icons/feather.min.js"></script>
+<script src="js/vendors.min.js"></script>
+<script src="../assets/icons/feather-icons/feather.min.js"></script>
 
 </body>
 </html>
