c038dfdb962fe2b7541ac0414ec168b46ab1f792
ashrafulmd389 <ashrafulmd389@gmail.com>
2024-09-05 23:37:13 +0600

    student_marksheet_genearate_module_part_5_completed

--- resources/views/marksheet/marksheet_pdf.blade.php
+++ resources/views/marksheet/marksheet_pdf.blade.php
@@ -35,6 +35,57 @@
                                     <p style="text-align: right"><u><i>Print Date: </i>{{ date('d M Y') }}</u></p>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <table border="1" style="border-color: #FFFFFF" width="100%;" cellpadding="8" cellspacing="2">
+                                        @php
+                                            $assignStudent = \App\Models\AssignStudent::where(['year_id'=>$allMarks['0']->year_id,'class_id'=>$allMarks['0']->class_id])->first();
+                                        @endphp
+                                        <tr>
+                                            <td width="50%">Student ID</td>
+                                            <td width="50%">{{ $allMarks['0']['id_number'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td width="50%">Roll No.</td>
+                                            <td width="50%">{{ $assignStudent->roll }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td width="50%">Student Name</td>
+                                            <td width="50%">{{ $allMarks['0']['student']->name }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td width="50%">Session</td>
+                                            <td width="50%">{{ $allMarks['0']['studentYear']->year_name }}</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="col-md-6">
+                                    <table border="1" style="border-color: #FFFFFF" width="100%;" cellpadding="8" cellspacing="2">
+                                        <thead>
+                                        <tr>
+                                            <td>Letter Grade</td>
+                                            <td>Marks Interval</td>
+                                            <td>Grade Point</td>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($allGrades as $grade)
+                                            <tr>
+                                                <td>{{ $grade->grade_name }}</td>
+                                                <td>{{ $grade->start_marks }} - {{ $grade->end_marks }}</td>
+                                                <td>
+                                                    {{ number_format((float)$grade->grade_point,2) }} - {{ ($grade->grade_point == 5)?
+                                                 (number_format((float)$grade->grade_point,2)):
+                                                 (number_format((float)$grade->grade_point+1,2) - (float)0.01) }}
+                                                </td>
+                                            </tr>
+                                        @endforeach
+
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
 
